54e913c6b518638da110b9f52520222686e13ea3
KeMaKone <atpgamer@gmail.com>
2022-02-25 17:37:06 +0200

    implemented intropage

--- src/Pages/IntroductionPage.js
+++ src/Pages/IntroductionPage.js
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GridTile from "../Components/GridTile";
 
-class IntroductionPage extends React.Component{
-    render() {
-        return <div>
-            <ul>
+const baseApiCall = 'https://pokeapi.co/api/v2/pokemon/';
 
-            </ul>
-        </div>;
+var limit = 3;
+
+
+function generateRandoms(){
+    const maxPokemonIndex = 898;
+    const nums = [];
+    for(var i = 0; i < limit; i++){
+        nums[i] = Math.floor(Math.random() * maxPokemonIndex + 1);
     }
+    return nums;
+}
+
+function getNewRoster(){
+    let pokemons = [];
+    const nums = generateRandoms();
+    nums.forEach(id => {
+        pokemons.push(fetch(baseApiCall+id).then(res => res.json()));
+    });
+    return pokemons;    
+}
+
+const IntroductionPage = () =>{
+    const [pokemons, setPokemons] = useState([]);
+
+    useEffect(() => {
+        setPokemons(getNewRoster())
+    }, [])
+
+    console.log(pokemons)
+    return(
+        <ul>
+            { pokemons.map((pokemon, idx) => (
+                <GridTile key={idx} obj={pokemon} />
+            ))}
+        </ul>
+    );
 }
 
 export default IntroductionPage;
